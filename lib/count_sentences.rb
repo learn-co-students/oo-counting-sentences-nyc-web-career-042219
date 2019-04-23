cad8e99d8512588dc0a1b3789ae13889ac435860
Carly Pecora <carly@carlypecora.com>
2019-04-23 16:44:05 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,30 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+  	self.end_with?("?")
   end
 
   def exclamation?
-
+  	self.end_with?("!")
   end
 
   def count_sentences
-
+  	# binding.pry
+  	new_arr = []
+  	count = 0
+  	arr = self.split(/(?<=[?.!])\s*/)
+  	for sentence in arr
+  		if "abcdefghijklmnopqrstuvwxyz".split('').any? &sentence.downcase.method(:include?)
+  			count += 1
+  		end
+  	end
+  	count
   end
+
 end
+
+# binding.pry
