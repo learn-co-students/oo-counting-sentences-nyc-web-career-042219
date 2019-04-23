ca06573fa4a3b27ae34dd45186d82cadc70f056a
Yasmine <y.hartung@gmail.com>
2019-04-23 17:50:03 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,44 @@ require 'pry'
 class String
 
   def sentence?
-    
+
+     if self.reverse[0,1] == "."
+       return true
+     else
+       return false
+     end
+
   end
 
   def question?
+
+    if self.reverse[0,1] == "?"
+      return true
+    else
+      return false
+    end
 
   end
 
   def exclamation?
 
+    if self.reverse[0,1] == "!"
+      return true
+    else
+      return false
+    end
+
   end
 
   def count_sentences
+    arr = self.chars
+    count = 0
+    arr.each_with_index do |char, index|
 
+      if ( char == "." || char == "!" || char == "?" ) && ( arr[index-1].match(/[a-zA-Z]/)  )
+        count = count +1
+      end
+    end
+    count
   end
 end
