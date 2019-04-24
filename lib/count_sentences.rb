384d2f070b59e4794b90c37b4c15f95e7a514520
Kane Koo <kanekoo@gmail.com>
2019-04-23 23:04:34 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,40 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    sent_arr=[]
+    count=0
+    sent_arr=self.split('.')
+#    binding.pry
+    sent_arr.each do |frag|
+      if frag != ""
+          frag_arr=frag.split('?')
+          frag_arr.each do |subfrag|
+            if subfrag != ""
+              subfrag_arr=subfrag.split('!')
+              subfrag_arr.each do |token|
+                if token != ""
+                  count +=1
+  #                binding.pry
+                end
+              end
+            end
+          end
+        end
 
+      end
+      binding.pry
+      return(count)
   end
 end
