7aa00162d7d3df6de7d21110291be0abed269106
Arslan Ali <ali.arslan319@gmail.com>
2019-05-05 23:54:44 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
+    #does this mean we're telling #sentence to be explicitly aware of itself?
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/[.?!]+/).count
   end
+
 end
